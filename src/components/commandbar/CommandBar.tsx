5a91e7e6e128299e36e2a174a21b9031bc57f296
Peter Fillo <peter.fillo@bwtplus.sk>
2021-07-11 17:29:01 +0200

    Optimise CommandBar rendering

--- src/components/commandbar/CommandBar.tsx
+++ src/components/commandbar/CommandBar.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Omnibar } from '@blueprintjs/select';
+import { useState, useCallback, useMemo } from 'react';
+import { Omnibar, ItemRenderer, ItemListRenderer, ItemPredicate } from '@blueprintjs/select';
 import { Menu, MenuItem, InputGroupProps2 } from '@blueprintjs/core';
 import { includesIgnoreCase } from '../../utils/stringUtils';
 import Highlighter from '../Highlighter';
@@ -7,19 +7,13 @@ import useCommandEngine from './useCommandEnging';
 import { Command } from './command';
 import root from './commands/root';
 
-const ControlMenu = ({ text }: { text: string }) => (
-  <Menu>
-    <MenuItem disabled={true} text={text} />
-  </Menu>
-);
-
 const CommandOmmibar = Omnibar.ofType<Command>();
 
 const CommandBar = () => {
   const [isOpen, setOpen] = useState(true);
   const { command, commandResult, commandHistory, pushCommand, popCommand } = useCommandEngine(root);
 
-  const inputProps = (): InputGroupProps2 => {
+  const inputProps: InputGroupProps2 = useMemo(() => {
     // const placeholderCrumbs = (icon: IconName): JSX.Element => {
     //   const previous = commandHistory[0]?.key !== "root" ? ;
     //   return (
@@ -48,9 +42,9 @@ const CommandBar = () => {
           placeholder: commandResult.error,
         };
     }
-  };
+  }, [commandResult, command]);
 
-  const items = () => {
+  const items = useMemo(() => {
     switch (commandResult.type) {
       case 'loading':
         return [];
@@ -59,7 +53,7 @@ const CommandBar = () => {
       case 'error':
         return [];
     }
-  };
+  }, [commandResult]);
 
   const handleSelectItem = (command: Command): void => {
     console.log('select command', command);
@@ -73,39 +67,53 @@ const CommandBar = () => {
     }
   };
 
+  const itemRenderer: ItemRenderer<Command> = useCallback((command, { handleClick, modifiers, query }) => {
+    return (
+      <MenuItem
+        active={modifiers.active}
+        text={<Highlighter text={command.title} query={query} />}
+        label={command.label}
+        key={command.key}
+        onClick={handleClick}
+      />
+    );
+  }, []);
+
+  const itemListRenderer: ItemListRenderer<Command> = useCallback(({ renderItem, items, filteredItems }) => {
+    if (items.length === 0) {
+      return (
+        <Menu>
+          <MenuItem disabled={true} text="No available commands" />
+        </Menu>
+      );
+    }
+    if (filteredItems.length === 0) {
+      return <Menu></Menu>;
+    }
+    return <Menu>{filteredItems.map(renderItem)}</Menu>;
+  }, []);
+
+  const itemPredicate: ItemPredicate<Command> = useCallback((query, { title }) => includesIgnoreCase(title, query), []);
+
+  const handleClose = useCallback(() => {
+    if (commandHistory.length > 0) {
+      popCommand();
+    } else {
+      setOpen(false);
+    }
+  }, [commandHistory, popCommand]);
+
   return (
     <CommandOmmibar
-      inputProps={inputProps()}
+      inputProps={inputProps}
       isOpen={isOpen}
-      onClose={(event) => {
-        if (commandHistory.length > 0) {
-          popCommand();
-        } else {
-          setOpen(false);
-        }
-      }}
-      items={items()}
+      onClose={handleClose}
+      items={items}
       itemsEqual="key"
       onItemSelect={handleSelectItem}
-      itemPredicate={(query, { title }) => includesIgnoreCase(title, query)}
-      itemListRenderer={({ renderItem, items, filteredItems }) => {
-        if (items.length === 0) {
-          return <ControlMenu text="No available commands" />;
-        }
-        if (filteredItems.length === 0) {
-          return <ControlMenu text="No matching search commands" />;
-        }
-        return <Menu>{filteredItems.map(renderItem)}</Menu>;
-      }}
-      itemRenderer={(command, { handleClick, modifiers, query }) => (
-        <MenuItem
-          active={modifiers.active}
-          text={<Highlighter text={command.title} query={query} />}
-          label={command.label}
-          key={command.key}
-          onClick={handleClick}
-        />
-      )}
+      itemPredicate={itemPredicate}
+      itemListRenderer={itemListRenderer}
+      itemRenderer={itemRenderer}
     />
   );
 };
