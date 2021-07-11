33c63244b9597786cebe9fa9caabe4a53546d9b8
Peter Fillo <peter.fillo@bwtplus.sk>
2021-07-11 22:17:15 +0200

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AppConfigContextProvider } from './components/hooks/useConfig';
-import { Route } from 'react-router';
-import { Switch } from 'react-router';
-import './App.css';
 import Omnibar from './components/omnibar';
 import CommandBar from './components/commandbar/CommandBar';
 import PageHome from './pages/PageHome';
 import PageOne from './pages/PageOne';
 import PageTwo from './pages/PageTwo';
+import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <AppConfigContextProvider>
-        <div className="App">
-          <Switch>
-            <Route path="/one" component={PageOne} />
-            <Route path="/two" component={PageTwo} />
-            <Route component={PageHome} />
-          </Switch>
-          <Omnibar />
-          <CommandBar />
-        </div>
-      </AppConfigContextProvider>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <AppConfigContextProvider>
+      <div className="App">
+        <Switch>
+          <Route path="/one" component={PageOne} />
+          <Route path="/two" component={PageTwo} />
+          <Route component={PageHome} />
+        </Switch>
+        <Omnibar />
+        <CommandBar />
+      </div>
+    </AppConfigContextProvider>
+  </BrowserRouter>
+);
 
 export default App;
