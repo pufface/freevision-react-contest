d67acedcb93fd1f1c6afae1418d76f71ab1657a2
Peter Fillo <peter.fillo@bwtplus.sk>
2021-07-24 23:50:58 +0200

    Fix remote search race condintions

--- src/components/commandbar/engine/useCommandFetcher.tsx
+++ src/components/commandbar/engine/useCommandFetcher.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useMemo } from 'react';
 import { SelectorCommand, Command, QuerySelectorCommand, SimpleSelectorCommand } from './command';
 import { Result, LoadingResult, SuccessResult, ErrorResult } from '../../../utils/result';
 import { includesIgnoreCase } from '../../../utils/stringUtils';
-import { debounce } from 'lodash';
+import debounce from 'debounce-promise';
 
 const DEBOUNCE_WAIT_MS = 250;
 
@@ -19,41 +19,44 @@ const emptyOptions = <T,>(): Options<T> => ({
 const useCommandFetcher = <T,>(command: SelectorCommand<T>, query: string) => {
   const [result, setResult] = useState<Result<Options<T>, string>>(LoadingResult());
 
-  const fetchQueryOptions = useMemo(() => {
-    return (selector: QuerySelectorCommand<T>, searchQuery: string) => {
-      Promise.resolve()
-        .then(() => setResult(LoadingResult()))
-        .then(() => selector.options(searchQuery))
-        .then((options) => setResult(SuccessResult(options)))
-        .catch(() => setResult(ErrorResult('Error fetching')));
-    };
-  }, []);
-
-  const fetchQueryOptionsDebounced = useMemo(() => {
-    return debounce(fetchQueryOptions, DEBOUNCE_WAIT_MS);
-  }, [fetchQueryOptions]);
-
-  const getSimpleOptions = useMemo(() => {
-    return (selector: SimpleSelectorCommand<T>, searchQuery: string) => {
-      const allOptions = selector.options();
-      const filteredOptions = allOptions.filter(({ title }) => includesIgnoreCase(title, searchQuery));
-      const options = {
-        totalCount: filteredOptions.length,
-        options: filteredOptions,
+  const optionsFetcher = useMemo(() => {
+    const simpleSelectorFetcher = (command: SimpleSelectorCommand<T>) => {
+      const allOptions = command.options();
+      return (query: string) => {
+        const filteredOptions = allOptions.filter(({ title }) => includesIgnoreCase(title, query));
+        return {
+          totalCount: filteredOptions.length,
+          options: filteredOptions,
+        };
       };
-      setResult(SuccessResult(options));
     };
-  }, []);
+    const querySelectorFetcher = (command: QuerySelectorCommand<T>) => {
+      return (query: string) => command.options(query);
+    };
+    return command.type === 'simpleSelector'
+      ? simpleSelectorFetcher(command)
+      : debounce(querySelectorFetcher(command), DEBOUNCE_WAIT_MS, { leading: true });
+  }, [command]);
 
   useEffect(() => {
-    if (command.type === 'simpleSelector') {
-      getSimpleOptions(command, query);
-    } else if (query === '') {
-      fetchQueryOptions(command, query);
-    } else {
-      fetchQueryOptionsDebounced(command, query);
-    }
-  }, [command, query, getSimpleOptions, fetchQueryOptions, fetchQueryOptionsDebounced]);
+    let cancelled = false;
+    Promise.resolve()
+      .then(() => setResult(LoadingResult()))
+      .then(() => optionsFetcher(query))
+      .then((options) => {
+        if (!cancelled) {
+          setResult(SuccessResult(options));
+        }
+      })
+      .catch(() => {
+        if (!cancelled) {
+          setResult(ErrorResult('Error fetching'));
+        }
+      });
+    return () => {
+      cancelled = true;
+    };
+  }, [optionsFetcher, query]);
 
   return result;
 };
